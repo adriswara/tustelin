3202a284e234b6220e0157a438537f7d09be828b
Adriswara <chevellerz454@yahoo.com>
2022-03-16 16:09:45 +0700

    several error removed

--- app/Views/layout/page_layout.php
+++ app/Views/layout/page_layout.php
@@ -2,140 +2,140 @@
 <html lang="en">
 
 <head>
-    <title><?= $title; ?></title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" src="https://kit.fontawesome.com/a076d05399.js" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
-    <!--STYLING CSS-->
-    <?= $this->include('layout/style'); ?>
-    <!-- -->
+  <title><?= $title; ?></title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" src="https://kit.fontawesome.com/a076d05399.js" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
+  <!--STYLING CSS-->
+  <?= $this->include('layout/style'); ?>
+  <!-- -->
 </head>
 
 <body class="bg-secondary">
-    <!--NAVBAR-->
-    <?= $this->include('layout/navbar'); ?>
-    <!--Modal Sign Up-->
+  <!--NAVBAR-->
+  <?= $this->include('layout/navbar'); ?>
+  <!--Modal Sign Up-->
 
-    <!-- The Modal -->
-    <div class="modal" id="myModal">
-        <div class="modal-dialog modal-fullscreen-sm-down">
-            <div class="modal-content">
-                <!-- Modal Header -->
-                <div class="modal-header">
-                    <h2 class="modal-title">Register</h2>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                </div>
-
-                <!-- Modal body -->
-                <div class="modal-body">
-                    <label for="email"><b>Email</b></label>
-                    <input type="text" placeholder="Enter Email" name="email" id="email" required />
-
-                    <label for="usrname"><b>Username</b> </label>
-                    <input type="text" placeholder="Enter Username" name="username" id="username" required />
-                    <label for="psw"><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="psw" id="psw" required />
-
-                    <label for="psw-repeat"><b>Repeat Password</b></label>
-                    <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required />
-                </div>
-
-                <!-- Modal footer -->
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">
-                        Sign Up
-                    </button>
-                </div>
-
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
-                        Cancel
-                    </button>
-                </div>
-            </div>
+  <!-- The Modal -->
+  <div class="modal" id="myModal">
+    <div class="modal-dialog modal-fullscreen-sm-down">
+      <div class="modal-content">
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h2 class="modal-title">Register</h2>
+          <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
         </div>
-    </div>
 
+        <!-- Modal body -->
+        <div class="modal-body">
+          <label for="email"><b>Email</b></label>
+          <input type="text" placeholder="Enter Email" name="email" id="email" required />
 
-    <!--HEADER > CONTENT-->
-    <?= $this->renderSection('pageContent'); ?>
-    <!--CONTENT > FOOTER-->
+          <label for="usrname"><b>Username</b> </label>
+          <input type="text" placeholder="Enter Username" name="username" id="username" required />
+          <label for="psw"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="psw" id="psw" required />
 
-    <!-- Modal Login -->
-
-    <div id="id01" class="modal">
-        <form class="modal-content animate" action="/action_page.php" method="post">
-            <div class="imgcontainer">
-                <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-                <img src="/img_avatar1.png" alt="Avatar" class="avatar" />
-            </div>
-
-            <div class="container">
-                <label for="uname"><b>Username</b></label>
-                <input type="text" placeholder="Enter Username" name="uname" required />
-
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="psw" required />
-
-                <button class="btn btn-primary" type="submit">Login</button>
-                <label>
-                    <input type="checkbox" checked="checked" name="remember" /> Remember
-                    me
-                </label>
-            </div>
-
-            <div class="container" style="background-color: #f1f1f1">
-                <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">
-                    Cancel
-                </button>
-                <span class="psw">Forgot <a href="#">password?</a></span>
-            </div>
-        </form>
-    </div>
-    <div>
-        <div class="container mt-0 bg-secondary" style="margin-bottom: 90px">
-            <div class="row">
-                <div class="col-sm-4">
-                    <h3 style="margin-top: -20px; padding-top: 70px">
-                        Daftarkan Akunmu
-                    </h3>
-                    <p style="margin-bottom: auto">
-                        Daftar terlebih dahulu untuk memesan jasa.
-                    </p>
-                </div>
-                <div class="col-sm-4" style="margin-top: 50px">
-                    <h3 style="height: margin-top: -20px;padding top : 70px;">
-                        Negosiasi
-                    </h3>
-                    <p style="margin-bottom: auto">
-                        Negosiasikan harga sesuai dengan kebutuhanmu
-                    </p>
-                </div>
-                <div class="col-sm-4">
-                    <h3 style="margin-top: -20px; padding-top: 70px">
-                        Pilih jasa sesuai kebutuhanmu
-                    </h3>
-                    <p style="margin-bottom: auto">
-                        Pilih berbagai jenis jasa foto sesuai dengan konsep dan kebutuhan
-                        anda
-                    </p>
-                </div>
-            </div>
+          <label for="psw-repeat"><b>Repeat Password</b></label>
+          <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required />
         </div>
+
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" data-bs-dismiss="modal">
+            Sign Up
+          </button>
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
+            Cancel
+          </button>
+        </div>
+      </div>
     </div>
+  </div>
 
-    <script>
-        // Get the modal login
-        var modal = document.getElementById("id01");
 
-        // When the user clicks anywhere outside of the modal login, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        };
-    </script>
+  <!--HEADER > CONTENT-->
+  <?= $this->renderSection('pageContent'); ?>
+  <!--CONTENT > FOOTER-->
+
+  <!-- Modal Login -->
+
+  <div id="id01" class="modal">
+    <form class="modal-content animate" action="/action_page.php" method="post">
+      <div class="imgcontainer">
+        <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+        <img src="/img_avatar1.png" alt="Avatar" class="avatar" />
+      </div>
+
+      <div class="container">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required />
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required />
+
+        <button class="btn btn-primary" type="submit">Login</button>
+        <label>
+          <input type="checkbox" checked="checked" name="remember" /> Remember
+          me
+        </label>
+      </div>
+
+      <div class="container" style="background-color: #f1f1f1">
+        <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">
+          Cancel
+        </button>
+        <span class="psw">Forgot <a href="#">password?</a></span>
+      </div>
+    </form>
+  </div>
+  <div>
+    <div class="container mt-0 bg-secondary" style="margin-bottom: 90px">
+      <div class="row">
+        <div class="col-sm-4">
+          <h3 style="margin-top: -20px; padding-top: 70px">
+            Daftarkan Akunmu
+          </h3>
+          <p style="margin-bottom: auto">
+            Daftar terlebih dahulu untuk memesan jasa.
+          </p>
+        </div>
+        <div class="col-sm-4" style="margin-top: 50px">
+          <h3>
+            Negosiasi
+          </h3>
+          <p style="margin-bottom: auto">
+            Negosiasikan harga sesuai dengan kebutuhanmu
+          </p>
+        </div>
+        <div class="col-sm-4">
+          <h3 style="margin-top: -20px; padding-top: 70px">
+            Pilih jasa sesuai kebutuhanmu
+          </h3>
+          <p style="margin-bottom: auto">
+            Pilih berbagai jenis jasa foto sesuai dengan konsep dan kebutuhan
+            anda
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script>
+    // Get the modal login
+    var modal = document.getElementById("id01");
+
+    // When the user clicks anywhere outside of the modal login, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+  </script>
 </body>
 
 </html>
