1e4bf4a3c87cc21fc7926338ea07877567111e9b
Adriswara <chevellerz454@yahoo.com>
2022-04-07 23:02:57 +0700

    login moved variable

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -6,9 +6,27 @@ use CodeIgniter\Controller;
 
 class Dashboard extends Controller
 {
+    public $globalVariable = 'I am callable';
+
+    private $session = null;
     public function index()
     {
-        $session = session();
-        echo "Welcome back, " . $session->get('username_pengguna');
+        $this->session = session();
+        //  $namaLog = $session->get('username_pengguna');
+        $data['get_sess'] = $this->session->get('username_pengguna');
+        //echo $namaLog;
+        //$output = ob_get_contents();
+        //parse_str($output);
+        // echo $output;
+
+        //echo "Welcome back, "  ; //$session->get('username_pengguna');
+        //  $data['get_session'] = $this->$namaLog;
+        $data['global'] = $this->globalVariable;
+        //   $data = [
+
+        //     'title' => 'Loggedin',
+        //   'namaLoged' => $namaLog
+        // ];
+        echo view('logintest/dashboard', $data);
     }
 }
---
+++ app/Views/logintest/dashboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    welcome back <?= $get_sess ?>
+
+    <a href="http://localhost:8080/login/logout"><button class="btn btn-primary" type="button"> Logout </button></a>
+
+</body>
+
+</html>
