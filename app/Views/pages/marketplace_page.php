e017113d66ae5f7e8b217e541152c6261f76b4f7
adriswara <chevellerz454@yahoo.com>
2022-05-18 18:11:17 +0700

    add css for marketplace

--- app/Views/pages/marketplace_page.php
+++ app/Views/pages/marketplace_page.php
@@ -47,11 +47,11 @@
 </nav>
 <!-- nav 2 -->
 
-<div class="bg-secondary marketplace" style="margin-top: -1rem; margin-bottom: 1rem">
+<div class="bg-light marketplace">
   <!-- Card -->
-  <div class="bg-light py-5">
-    <div class="justify-content-center" style="margin-left: 20rem; margin-right: -10rem">
-      <h2 class="justify-content-center" style="margin-top: 1rem">
+  <div class=" py-5 container">
+    <div class="justify-content-center">
+      <h2 class="justify-content-center mt-2">
         Hasil Pencarian Anda
       </h2>
       <p class="justify-content-center">Pencarian :
@@ -103,274 +103,38 @@
     </div>
   </div>
 
-  <div class="bg-light container-fluid">
-    <div class="row hidden-md-up justify-content-center">
-      <?php foreach ($fotografer as $k) : ?>
+  <div class="marketplace__list bg-light container">
+    <div class="row hidden-md-up justify-content-between">
+      <?php foreach ($fotografer as $index => $k) : ?>
 
-        <!---->
-        <div class="col-md-2">
-          <!---->
 
-          <div class="card" style="width: 300px">
-            <div>
-              <img id="CardDi" class="card-img-top" src="/displaypic/<?= $k['displaypic']; ?>" alt="Card image" style="width: 100%" />
-              <div style="margin: -2rem; margin-left: 1rem; left: ">
-                <img id="CardDp" src="/displaypic/<?= $k['displaypic']; ?>" alt="Logo" style="width: 70px" class="rounded-pill" />
-              </div>
-            </div>
 
-            <!-- Card Body -->
-            <div class="card-body" style="margin-top: 1rem">
-              <p class="text-secondary" style="margin-left: 10rem">
-                <?= $k['harga']; ?>
-              </p>
-              <h4 class="card-title" style="margin-top: 1rem"><?= $k['nama']; ?></h4>
-              <p class="card-text">
-                <?= $k['akun_instagram']; ?>
-              </p>
-              <p class="card-text"> <?= $k['nama_aliran']; ?> </p>
-              <p>
-                <a href="/Profile/<?= $k['slug']; ?>"><button class="btn btn-primary">See Profile</button></a>
-              </p>
-            </div>
+        <div class="card px-0">
+          <div class="card__imgwrapper">
+            <img id="CardDi" class="card__img" src="/displaypic/<?= $k['displaypic']; ?>" alt="Card image" />
+            <img id="CardDp" src="/displaypic/<?= $k['displaypic']; ?>" alt="Logo" class="rounded-pill card__pp" />
           </div>
-          <div style="padding-top: 1rem"></div>
+
+          <!-- Card Body -->
+          <div class="card-body card__body">
+            <p class="harga">
+              <?= $k['harga']; ?>
+            </p>
+            <h4 class="card-title mb-0"><?= $k['nama']; ?></h4>
+            <p class="account card-text text-secondary ">
+              <?= $k['akun_instagram']; ?>
+            </p>
+            <p class="type type__<?= $k['nama_aliran']; ?>"> Foto <?= $k['nama_aliran']; ?> </p>
+            <p class="button">
+              <a href="/Login"><button class="btn btn-primary">See Profile</button></a>
+            </p>
+          </div>
         </div>
+        <?php if ($index % 3 == 0 && $index != 0) { ?>
+          <div class="w-100"></div>
+        <?php } ?>
       <?php endforeach; ?>
 
-      <!---->
-      <div class="col-md-2">
-        <!---->
-
-        <div class="card" style="width: 300px">
-          <div>
-            <img id="CardDi" class="card-img-top" src="/makanan.jpg" alt="Card image" style="width: 100%" />
-            <div style="margin: -2rem; margin-left: 1rem; left: ">
-              <img id="CardDp" src="/img_avatar1.png" alt="Logo" style="width: 70px" class="rounded-pill" />
-            </div>
-          </div>
-
-          <!-- Card Body -->
-          <div class="card-body" style="margin-top: 1rem">
-            <p class="text-secondary" style="margin-left: 10rem">
-              Rp500.000
-            </p>
-            <h4 class="card-title" style="margin-top: 1rem">John Doe</h4>
-            <p class="card-text">
-              Some example text some example text. John Doe is an architect
-              and engineer
-            </p>
-            <p>
-              <button href="#" class="btn btn-primary">See Profile</button>
-            </p>
-          </div>
-        </div>
-        <div style="padding-top: 1rem"></div>
-      </div>
-      <!---->
-
-      <div class="col-md-2">
-        <!---->
-        <div class="card" style="width: 300px">
-          <div>
-            <img id="CardDi" class="card-img-top" src="/makanan.jpg" alt="Card image" style="width: 100%" />
-            <div style="margin: -2rem; margin-left: 1rem; left: ">
-              <img id="CardDp" src="/img_avatar1.png" alt="Logo" style="width: 70px" class="rounded-pill" />
-            </div>
-          </div>
-
-          <!-- Card Body -->
-          <div class="card-body" style="margin-top: 1rem">
-            <p class="text-secondary" style="margin-left: 10rem">
-              Rp500.000
-            </p>
-            <h4 class="card-title" style="margin-top: 1rem">John Doe</h4>
-            <p class="card-text">
-              Some example text some example text. John Doe is an architect
-              and engineer
-            </p>
-            <p>
-              <button href="#" class="btn btn-primary">See Profile</button>
-            </p>
-          </div>
-        </div>
-        <div style="padding-top: 1rem"></div>
-      </div>
-      <!---->
-      <div class="col-md-2">
-        <!---->
-        <div class="card" style="width: 300px">
-          <div>
-            <img id="CardDi" class="card-img-top" src="/makanan.jpg" alt="Card image" style="width: 100%" />
-            <div style="margin: -2rem; margin-left: 1rem; left: ">
-              <img id="CardDp" src="/img_avatar1.png" alt="Logo" style="width: 70px" class="rounded-pill" />
-            </div>
-          </div>
-
-          <!-- Card Body -->
-          <div class="card-body" style="margin-top: 1rem">
-            <p class="text-secondary" style="margin-left: 10rem">
-              Rp500.000
-            </p>
-            <h4 class="card-title" style="margin-top: 1rem">John Doe</h4>
-            <p class="card-text">
-              Some example text some example text. John Doe is an architect
-              and engineer
-            </p>
-            <p>
-              <button href="#" class="btn btn-primary">See Profile</button>
-            </p>
-          </div>
-        </div>
-        <div style="padding-top: 1rem"></div>
-      </div>
-      <!---->
-      <div class="col-md-2">
-        <!---->
-        <div class="card" style="width: 300px">
-          <div>
-            <img id="CardDi" class="card-img-top" src="/makanan.jpg" alt="Card image" style="width: 100%" />
-            <div style="margin: -2rem; margin-left: 1rem; left: ">
-              <img id="CardDp" src="/img_avatar1.png" alt="Logo" style="width: 70px" class="rounded-pill" />
-            </div>
-          </div>
-
-          <!-- Card Body -->
-          <div class="card-body" style="margin-top: 1rem">
-            <p class="text-secondary" style="margin-left: 10rem">
-              Rp500.000
-            </p>
-            <h4 class="card-title" style="margin-top: 1rem">John Doe</h4>
-            <p class="card-text">
-              Some example text some example text. John Doe is an architect
-              and engineer
-            </p>
-            <p>
-              <button href="#" class="btn btn-primary">See Profile</button>
-            </p>
-          </div>
-        </div>
-        <div style="padding-top: 1rem"></div>
-      </div>
-      <!---->
-    </div>
-    <br />
-    <div class="row justify-content-center">
-      <!---->
-      <div class="col-md-2">
-        <!---->
-
-        <div class="card" style="width: 300px">
-          <div>
-            <img id="CardDi" class="card-img-top" src="/makanan.jpg" alt="Card image" style="width: 100%" />
-            <div style="margin: -2rem; margin-left: 1rem; left: ">
-              <img id="CardDp" src="/img_avatar1.png" alt="Logo" style="width: 70px" class="rounded-pill" />
-            </div>
-          </div>
-
-          <!-- Card Body -->
-          <div class="card-body" style="margin-top: 1rem">
-            <p class="text-secondary" style="margin-left: 10rem">
-              Rp500.000
-            </p>
-            <h4 class="card-title" style="margin-top: 1rem">John Doe</h4>
-            <p class="card-text">
-              Some example text some example text. John Doe is an architect
-              and engineer
-            </p>
-            <p>
-              <button href="#" class="btn btn-primary">See Profile</button>
-            </p>
-          </div>
-        </div>
-        <div style="padding-top: 1rem"></div>
-      </div>
-      <!---->
-      <div class="col-md-2">
-        <!---->
-        <div class="card" style="width: 300px">
-          <div>
-            <img id="CardDi" class="card-img-top" src="/makanan.jpg" alt="Card image" style="width: 100%" />
-            <div style="margin: -2rem; margin-left: 1rem; left: ">
-              <img id="CardDp" src="/img_avatar1.png" alt="Logo" style="width: 70px" class="rounded-pill" />
-            </div>
-          </div>
-
-          <!-- Card Body -->
-          <div class="card-body" style="margin-top: 1rem">
-            <p class="text-secondary" style="margin-left: 10rem">
-              Rp500.000
-            </p>
-            <h4 class="card-title" style="margin-top: 1rem">John Doe</h4>
-            <p class="card-text">
-              Some example text some example text. John Doe is an architect
-              and engineer
-            </p>
-            <p>
-              <button href="#" class="btn btn-primary">See Profile</button>
-            </p>
-          </div>
-        </div>
-        <div style="padding-top: 1rem"></div>
-      </div>
-      <!---->
-      <div class="col-md-2">
-        <!---->
-        <div class="card" style="width: 300px">
-          <div>
-            <img id="CardDi" class="card-img-top" src="/makanan.jpg" alt="Card image" style="width: 100%" />
-            <div style="margin: -2rem; margin-left: 1rem; left: ">
-              <img id="CardDp" src="/img_avatar1.png" alt="Logo" style="width: 70px" class="rounded-pill" />
-            </div>
-          </div>
-
-          <!-- Card Body -->
-          <div class="card-body" style="margin-top: 1rem">
-            <p class="text-secondary" style="margin-left: 10rem">
-              Rp500.000
-            </p>
-            <h4 class="card-title" style="margin-top: 1rem">John Doe</h4>
-            <p class="card-text">
-              Some example text some example text. John Doe is an architect
-              and engineer
-            </p>
-            <p>
-              <button href="#" class="btn btn-primary">See Profile</button>
-            </p>
-          </div>
-        </div>
-        <div style="padding-top: 1rem"></div>
-      </div>
-      <!---->
-      <div class="col-md-2">
-        <!---->
-        <div class="card" style="width: 300px">
-          <div>
-            <img id="CardDi" class="card-img-top" src="/makanan.jpg" alt="Card image" style="width: 100%" />
-            <div style="margin: -2rem; margin-left: 1rem; left: ">
-              <img id="CardDp" src="/img_avatar1.png" alt="Logo" style="width: 70px" class="rounded-pill" />
-            </div>
-          </div>
-
-          <!-- Card Body -->
-          <div class="card-body" style="margin-top: 1rem">
-            <p class="text-secondary" style="margin-left: 10rem">
-              Rp500.000
-            </p>
-            <h4 class="card-title" style="margin-top: 1rem">John Doe</h4>
-            <p class="card-text">
-              Some example text some example text. John Doe is an architect
-              and engineer
-            </p>
-            <p>
-              <button href="#" class="btn btn-primary">See Profile</button>
-            </p>
-          </div>
-        </div>
-        <div style="padding-top: 1rem"></div>
-      </div>
-      <!---->
     </div>
   </div>
-</div>
-<?= $this->endSection(); ?>
+  <?= $this->endSection(); ?>
