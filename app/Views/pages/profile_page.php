e87fc0bbadaa6450ffbe53827bfc8484663f4080
adriswara <chevellerz454@yahoo.com>
2022-06-08 00:33:07 +0700

    fix styling review

--- app/Views/pages/profile_page.php
+++ app/Views/pages/profile_page.php
@@ -179,39 +179,25 @@
     </div>
   </div>
 
-  <div class="container">
-    <!-- <div class="card" style="width: 300px">
-      <div>
-        <img id="CardDi" class="card-img-top" src="/equipment.jpg" alt="Card image" style="width: 100%" />
-        <div style="margin: -2rem; margin-left: 1rem; "></div>
-      </div>
+  <div class="container mt-5">
 
-      <!-- Card Body -->
-    <!-- <div class="card-body" style="margin-top: 1rem">
-        <h4 class="card-title" style="margin-top: 1rem">
-          Equipment List
-        </h4>
-        <p class="card-text">Main Camera : <?= $profil[0]['nama_alat']; ?></p>
-
-        <h5>Other Equipment</h5>
-        <p class="card-text">Excell tripod</p>
-        <p>DJI Ronin S Gimbal</p>
-        <p>
-          <button href="#" class="btn btn-primary">More</button>
-        </p>
-      </div>
-    </div> --> -->
-
-    <h2>User Review</h2>
+    <h3>User Review</h3>
 
     <?php foreach ($profil as $k) : ?>
 
       <div class="containerChat">
-        <img src="/img_avatar1.png" alt="Avatar" style="width: 100%" />
-        <h4><?= $k['username_pengguna']; ?></h4>
+        <div class="d-flex">
+          <img src="/img_avatar1.png" alt="Avatar" />
+          <div>
+            <h4><?= $k['username_pengguna']; ?></h4>
+            <div class="time-right"><?= $k['waktu_kirim']; ?> </div>
+          </div>
+          <span class=""><i class="fa fa-star" aria-hidden="true"></i> <?= $k['rating']; ?></span>
+        </div>
+
         <p><?= $k['review']; ?></p>
-        <span class="">Rating : <?= $k['rating']; ?></span>
-        <span class="time-right"><?= $k['waktu_kirim']; ?> </span>
+
+
       </div>
 
     <?php endforeach; ?>
@@ -294,39 +280,7 @@
 
 
 
-      <div class="containerChat darker">
 
-        <h4>Input Review</h4>
-        <div class="form-group">
-          <form action="/Profile/saveReview/<?= $profil[0]['slug']; ?>" method="post">
-            <?= csrf_field(); ?>
-            <!--  -->
-            <label for="review" class="form-label">Review :<?= $profil[0]['slug']; ?></label>
-            <input type="text" name="review" class="form-control" id="review" value="<?= old('review'); ?>">
-            <!--  -->
-            <div class="btn-group">
-              Rating
-              </button>
-              <div class="dropdown-menu" value="rating1">
-                <i class="fa fa-star" aria-hidden="true">1</i>
-              </div>
-              <div class="dropdown-menu" value="rating2">
-                <i class="fa fa-star" aria-hidden="true">2</i>
-              </div>
-              <div class="dropdown-menu" value="rating3">
-                <i class="fa fa-star" aria-hidden="true">3</i>
-              </div>
-              <div class="dropdown-menu" value="rating4">
-                <i class="fa fa-star" aria-hidden="true">4</i>
-              </div>
-              <div class="dropdown-menu" value="rating5">
-                <i class="fa fa-star" aria-hidden="true">5</i>
-              </div> -->
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-          </form>
-        </div>
-      </div>
 
 
 
